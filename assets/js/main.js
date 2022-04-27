83c6ed8faff4cd8fcd04e5e72c56afc12be7e968
DGorinov <gorinovdimitri@yandex.ru>
2022-04-27 18:03:11 +0300

    second part console prog

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,13 +9,29 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Последний из просмотренных фильмов?', ''),
-      b = prompt('Рейтинг', ''),
-      c = prompt('Последний из просмотренных фильмов?', ''),
-      d = prompt('Рейтинг', '');
+for (let i = 0; i < 2; i ++) {
+    const a = prompt('Последний из просмотренных фильмов?', ''),
+          b = prompt('Рейтинг', '');
+    // personalMovieDB.movies [a] = b;
 
-personalMovieDB.movies [a] = b;
-personalMovieDB.movies [c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies [a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }   
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено слишком мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
 
